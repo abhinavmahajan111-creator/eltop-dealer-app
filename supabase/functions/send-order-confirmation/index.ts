7e13e7dd45ba0b7227f745b5748a7d2bb96145b1
Eltop Dealer App <msumaksh@gmail.com>
2026-07-10 11:19:09 +0530

    fix: allow x-client-info and apikey in Edge Function CORS headers

--- supabase/functions/send-order-confirmation/index.ts
+++ supabase/functions/send-order-confirmation/index.ts
@@ -23,7 +23,7 @@ serve(async (req) => {
     return new Response("ok", {
       headers: {
         "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Headers": "authorization, content-type",
+        "Access-Control-Allow-Headers": "authorization, x-client-info, apikey, content-type",
       },
     });
   }
